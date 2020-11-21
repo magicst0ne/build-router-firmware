13fb7218424a32c95984ea78772c65ad4a818d96
stone <magicstone106@gmail.com>
2020-11-21 15:33:43 +0800

    change name

--- build-merlin-ubuntu/build-ac88u.sh
+++ build-merlin-ubuntu/build-ac88u.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-### Build RT-AC68U firmware (ARM)
+### Build RT-AC88U firmware (ARM)
 export LD_LIBRARY_PATH=
 export TOOLCHAIN_BASE=
 echo $PATH | grep -qF /opt/brcm-arm/bin || export PATH=$PATH:/opt/brcm-arm/bin
 echo $PATH | grep -qF /opt/brcm-arm/arm-brcm-linux-uclibcgnueabi/bin || export PATH=$PATH:/opt/brcm-arm/arm-brcm-linux-uclibcgnueabi/bin
-cd ~/asuswrt-merlin.ng-build/release/src-rt-6.x.4708
-make rt-ac68u
+cd ~/asuswrt-merlin.ng-build/release/src-rt-7.14.114.x/src
+make clean
+make rt-ac88u
--- build-merlin-ubuntu/update-merlin.sh
+++ build-merlin-ubuntu/update-merlin.sh
@@ -4,18 +4,15 @@
 cd
 if [ ! -d asuswrt-merlin.ng ]; then
   git clone https://github.com/RMerl/asuswrt-merlin.ng
-  cd asuswrt-merlin.ng
-  git checkout mainline
 else
   cd asuswrt-merlin.ng
-  git checkout mainline
+  git clean -fdxq && git reset --hard
   git pull
 fi
 
 ### Create a build tree from the repo and fix the symlinks
 cd
 rm -rf asuswrt-merlin.ng-build
-#rsync -aq asuswrt-merlin.ng/ asuswrt-merlin.ng-build --exclude .git
 cp -a asuswrt-merlin.ng/ asuswrt-merlin.ng-build
 
 # fix symlinks for HND toolchain
